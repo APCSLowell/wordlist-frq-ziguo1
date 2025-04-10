98702b896b2ce32a65337a14bd5263bbf2a9d7f0
ziguo1 <ziguo1@s.sfusd.edu>
2025-04-10 18:10:58 +0000

    wAAAaaAaAAaaAaAa

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -11,12 +11,19 @@ public class WordList
   public int numWordsOfLength(int len)
   {
     /* to be implemented in part (a) */
-    
+    int cnt = 0;
+    for (String ent : myList) {
+      if (ent.length() == len) cnt++;
+    }
+    return cnt;
   }
 
   public void removeWordsOfLength(int len)
   {
     /* to be implemented in part (b) */
-
+    for (int i = myList.size() - 1; i >= 0; i--) {
+      if (myList.get(i).length() == len)
+        myList.remove(i);
+    }
   }
 }
